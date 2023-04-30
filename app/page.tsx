4ed9ed9f34fd55840e5a98a2f3d0ec2ec9d09076
brcelso <celso.sjunior@outlook.com>
2023-04-29 22:28:49 -0300

    ok

--- app/page.tsx
+++ app/page.tsx
@@ -23,24 +23,25 @@ import { Calendar } from "@/components/ui/calendar"
 
 export default function IndexPage() {
   return (
-    <div className="container relative pb-10">
-      
+    <>
+      <section className="space-y-6">
+      <div className="container flex max-w-[64rem] flex-col items-center gap-4 text-center">
+
       <StyleSwitcher />
       <PageHeader>
-        <PageHeaderHeading>Welcome to the  New Age.=D</PageHeaderHeading>
+        <PageHeaderHeading>Welcome to the  New Age.</PageHeaderHeading>
         <PageHeaderDescription>
           This is my page.
           Just Code.
         </PageHeaderDescription>
       </PageHeader>
+      </div>
       
       <div className="overflow-hidden rounded-lg">
           <TradingViewWidget />
         </div>
-
-      <section className="space-y-6 pb-8 pt-6 md:pb-12 md:pt-10 lg:py-32 border">
+      
         <div className="container flex max-w-[64rem] flex-col items-center gap-4 text-center">
-        
       <Calendar />
       {/*<Avatar>
       <AvatarImage src="https://github.com/brcelso.png" />
@@ -116,7 +117,7 @@ export default function IndexPage() {
 
       <ExamplesNav className="[&>a:first-child]:text-primary" />
       
-        </div>
+        </>
   )
 }
         {/*<section className="overflow-hidden rounded-lg border bg-background shadow-xl">
