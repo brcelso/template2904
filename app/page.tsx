8a5341a3cd96c95489f4211d4e507311ba64da5b
brcelso <celso.sjunior@outlook.com>
2023-04-29 23:10:33 -0300

    ok

--- app/page.tsx
+++ app/page.tsx
@@ -25,7 +25,7 @@ export default function IndexPage() {
   return (
     <>
     <section className="space-y-6 pb-8 pt-6 md:pb-12 md:pt-10 lg:py-32">
-        <div className="container flex max-w-[64rem] flex-col items-center gap-4 text-center">
+        <div className="overflow-hidden rounded-lg container flex max-w-[64rem] flex-col items-center gap-4 text-center">
       {/*<section className="space-y-6">
       <div className=" overflow-hidden rounded-lg container flex max-w-[64rem] flex-col items-center gap-4 text-center">
 
@@ -41,7 +41,6 @@ export default function IndexPage() {
           <p className="max-w-[42rem] leading-normal text-muted-foreground sm:text-xl sm:leading-8">
             The future is Coding.
           </p>
-      
       
           <TradingViewWidget />
           </div>
